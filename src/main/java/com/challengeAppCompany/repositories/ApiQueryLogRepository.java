95173e819fc7d6dfb4b719413fe8b3a08c18dbd0
Renan Reis <renanmacfernandes@hotmail.com>
2024-11-30 00:37:24 -0300

    creation of service class ApiQueryLogService

--- src/main/java/com/challengeAppCompany/repositories/ApiQueryLogRepository.java
+++ src/main/java/com/challengeAppCompany/repositories/ApiQueryLogRepository.java
@@ -1,9 +1,11 @@
 package com.challengeAppCompany.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.challengeAppCompany.entities.ApiQueryLog;
 
+@Repository
 public interface ApiQueryLogRepository extends JpaRepository<ApiQueryLog, Long> {
 
 }
---
+++ src/main/java/com/challengeAppCompany/services/ApiQueryLogService.java
@@ -0,0 +1,26 @@
+package com.challengeAppCompany.services;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.challengeAppCompany.entities.ApiQueryLog;
+import com.challengeAppCompany.repositories.ApiQueryLogRepository;
+
+@Service
+public class ApiQueryLogService {
+	
+	@Autowired
+	private ApiQueryLogRepository repository;
+	
+	public void saveLog(String cep, String response, String status) {
+		ApiQueryLog log = new ApiQueryLog();
+		log.setQueryTime(LocalDateTime.now());
+		log.setCep(cep);
+		log.setStatus(status);
+		
+		repository.save(log);
+	}
+
+}
