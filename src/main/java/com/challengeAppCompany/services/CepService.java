ccc7fd7921299dcf3862af2577a7c2c78d98da01
Renan Reis <renanmacfernandes@hotmail.com>
2024-11-30 01:04:09 -0300

    creation of service class CepService

---
+++ src/main/java/com/challengeAppCompany/services/CepService.java
@@ -0,0 +1,47 @@
+package com.challengeAppCompany.services;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class CepService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private ApiQueryLogService logService;
+
+	private static final String EXTERNAL_API_URL = "https://viacep.com.br/ws/{cep}/json";
+	
+	public Map<String, Object> getAddressByCep(String cep){
+		log.info("Iniciando consulta para o cep: {}", cep);
+		
+		try {
+			//CALLING EXTERNAL API
+			ResponseEntity<Map> responseEntity = restTemplate.getForEntity(EXTERNAL_API_URL, Map.class, cep);
+			
+			Map<String, Object> responseBody = responseEntity.getBody();
+			log.info("Consulta concluída com sucesso: {}", responseBody);
+			
+			//SAVING THE LOG IN THE DATABASE
+			logService.saveLog(cep, responseBody.toString(), "SUCCESS");
+			
+			return responseBody;
+		}catch (HttpClientErrorException.NotFound e) {
+			log.warn("Nenhum endereço encontrado para o CEP: {}", cep);
+			logService.saveLog(cep, e.getMessage(), "ERROR");
+			
+			throw e;
+		}
+		
+	}
+}
